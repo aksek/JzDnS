274bff6c27a2136240d141a667af4c1aa617db03
aksek <kosekaniela@gmail.com>
2021-06-05 22:54:02 +0200

    started writing a test for solution

--- test/looper_test.cpp
+++ test/looper_test.cpp
@@ -135,3 +135,45 @@ BOOST_AUTO_TEST_CASE(get_riddle_test) {
     BOOST_CHECK_EQUAL(decrypted, "why");
     looper.stop();
 }
+
+BOOST_AUTO_TEST_CASE(solution_test) {
+    Riddle riddle(0, "why", "because");
+
+    CryptoPP::AutoSeededRandomPool rng;
+    CryptoPP::RSA::PublicKey public_key;
+    Cryptography::load_public_key(public_key, "./public_key.pem");
+
+    CryptoPP::RSA::PublicKey user_public_key;
+    CryptoPP::RSA::PrivateKey user_private_key;
+    Cryptography::generate_public_private_key(user_public_key, user_private_key, rng);
+
+
+    UserBase userBase;
+    userBase.addUser(User("a", User::UserType::NORMAL, user_public_key));
+
+    Authorization authorization(&userBase);
+    RiddleBase riddleBase;
+    riddleBase.addRiddle(riddle);
+    RiddleModule riddleModule(&riddleBase);
+    AdminModule adminModule(&riddleBase);
+
+    QueueMap userQueues(&authorization);
+    BlockingQueue<Message> queue;
+    userQueues.add_user("a", &queue);
+
+    SerializeContent serializer;
+
+    Looper looper(&userQueues, &authorization, &riddleModule, &adminModule);
+    BOOST_TEST_CHECKPOINT( "Looper initiated");
+    looper.run();
+    BOOST_TEST_CHECKPOINT( "Looper running");
+    looper.getDispatcher()->post(Message(MessageType::Get_current_problem, "a"));
+    Message result = userQueues.pop("a");
+
+    BOOST_CHECK(result.getMessageType() == MessageType::Problem);
+    auto result_content = result.getContent();
+    std::string result_deserialized = std::get<std::string>(serializer.deserialize(MessageType::Problem, result_content));
+    std::string decrypted = Cryptography::asymmetric_decrypt(user_private_key, result_deserialized, rng);
+    BOOST_CHECK_EQUAL(decrypted, "why");
+    looper.stop();
+}
